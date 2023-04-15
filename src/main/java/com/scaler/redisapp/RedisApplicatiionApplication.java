2c265154e0cb8745754d3e91a225546bad74040f
IShahnawazShaikh <shahnawazexplore@gmail.com>
2023-04-15 17:42:26 +0530

    Initialized Project

---
+++ src/main/java/com/scaler/redisapp/RedisApplicatiionApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.redisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisApplicatiionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplicatiionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/redisapp/RedisApplicatiionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.redisapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicatiionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
